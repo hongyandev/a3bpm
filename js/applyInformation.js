9ad9269683873a8cb70532849813ca73c757be2d
Chai <chai@fy.com>
2019-06-14 13:52:34 +0800

    a3bpm

--- js/applyInformation.js
+++ js/applyInformation.js
@@ -102,7 +102,7 @@ $(function () {
                             "           <div class=\"weui-cell\">\n" +
                             "               <div class=\"weui-cell__bd\">\n";
                         if(val.ShenPiYJ){
-                            spFjLists+="<textarea style=\"height:2rem;\" class=\"weui-textarea\" rows=\"3\">"+val.ShenPiYJ+"</textarea>\n";
+                            spFjLists+="<textarea style='height:2rem;font-size:16px;' class=\"weui-textarea\" rows=\"3\">"+val.ShenPiYJ+"</textarea>\n";
                         }
                         spFjLists+="                      <div class=\"weui-textarea-counter clearfix\"><label class=\"fl\">处理时长："+val.ChuLiSC+"</label><span class=\"fr\">"+val.ShenPiR+"</span></div>\n" +
                             "                      <div class=\"weui-textarea-counter\"><time>"+val.ShenPiSJ+"</time></div>\n" +
