a6df74856611a535c6ee2d108257bce0536f43c3
毛袁晨 <maoyuanchen@gmail.com>
2019-06-20 19:23:45 +0800

    index

--- js/testData.js
+++ js/testData.js
@@ -2,37 +2,37 @@ var testData = {
     "msgCode": 1,
     "msg": "成功",
     "DanJuXX": {
-        "DanWeiMC": "单位名称",
-        "BuMenMC": "部门名称",
-        "ShenQingBMBH": "申请部门编号",
-        "JieKuanR": "借款人",
-        "JieKuanDLX": "借款单类型",
-        "ShenQingRQ": "申请日期",
-        "JieKuanJE": "借款金额",
-        "ZhiChuSX": "支出事项",
-        "ZhiChuSXBH": "支出事项编号",
-        "ZhiBiaoBH": "指标编号",
-        "ZhiBiaoMC": "指标名称",
-        "KeYongYE": "可用余额",
-        "LiuChengTuLJ": "流程图路径",
-        "ShiYou": "事由",
-        "HeTongMC": "合同名称",
-        "ZhiXingSQD": "执行申请单据号",
-        "JingBanR": "经办人",
-        "JingBanRBH": "经办人编号",
-        "JieKuanBMBH": "借款部门编号",
-        "JieKuanBM": "借款部门名称",
+        "JieKuanR": "操作管理员",
+        "ZhiChuSX": "办公设备购置费",
+        "ShiYou": "合同审批测试",
+        "ZhiXingSQD": "C309001-20190510-01F",
+        "KeYongYE": "5,000.00",
+        "JieKuanJE": "5,000.00",
+        "ZhiBiaoMC": "信息化运行维护经费-信息化系统运行维护管理",
+        "ZhiChuSXBH": "ZCSX201903240064",
+        "ShenQingBMBH": "GLZZ201905250002",
+        "JieKuanBMBH": "ZZJG201905230004",
+        "JieKuanDLX": "合同资金申请",
+        "BuMenMC": "办公室",
+        "HeTongMC": "关于xxx专项设备的采购",
+        "JingBanRBH": "XTYH201905300002",
+        "DanWeiMC": "浙江省*****",
+        "ShenQingRQ": "2019-06-19",
+        "ZhiBiaoBH": "NPZB201905270002",
+        "JieKuanBM": "办公室",
+        "LiuChengTuLJ": "http://119.18.195.153:8088/bpm/image/2019318155749.510217/2019318155749.510217.png",
+        "JingBanR": "操作管理员",
         "DanJuFJ": [
             {
-                "FuJianMC": "XXX.doc",
-                "FuJianLJ": "xxx/xxx/xxx.doc"
+                "FuJianMC": "02-首页-02-1.png",
+                "FuJianLJ": "http://119.18.195.153:8088/bpm/image//upload/zhijiejk/1560926414811/FJ20190619000001.png"
             }
         ]
     },
     "ZhiChuMX": [
         {
-            "MingXiMC": "明细名称",
-            "MingXiJE": "明细金额"
+            "MingXiJE": "5,000.00",
+            "MingXiMC": "办公设备购置费"
         }
     ],
     "FuZhuXX": [
@@ -43,16 +43,16 @@ var testData = {
     ],
     "JieSuanXX": [
         {
-            "JieSuanFS": "结算方式",
-            "JieSuanJE": "结算金额",
-            "GongWuKH": "公务卡号",
-            "JiZhangRQ": "记账日期",
-            "XiaoFeiRQ": "消费日期",
-            "HuanKuanRQ": "还款日期",
-            "BeiZhu": "备注",
-            "ShouKuanFZHM": "收款方账户名",
-            "KaiHuH": "开户行",
-            "ZhangHao": "账号"
+            "BeiZhu": "",
+            "HuanKuanRQ": "",
+            "JieSuanJE": "5,000.00",
+            "JiZhangRQ": "",
+            "JieSuanFS": "转账",
+            "XiaoFeiRQ": "",
+            "KaiHuH": "xxx",
+            "GongWuKH": "",
+            "ShouKuanFZHM": "北京xx技术设备服务商",
+            "ZhangHao": "xxxx3"
         }
     ],
     "ShenPiXX": [
@@ -74,6 +74,33 @@ var testData = {
                     "FuJianLJ": "xxx/xxx/xxx.doc"
                 }
             ]
+        },
+        {
+            "ChuLiSC": "即时",
+            "ShenPiR": "单位财务负责人(岗位)",
+            "ShenPiYS": [],
+            "ShenPiSJ": "",
+            "ShenPiFJ": {},
+            "ShenPiHJ": "单位财务负责人",
+            "ShenPiYJ": ""
+        },
+        {
+            "ChuLiSC": "即时",
+            "ShenPiR": "操作管理员",
+            "ShenPiYS": [],
+            "ShenPiSJ": "2019-06-19 14:41:50",
+            "ShenPiFJ": {},
+            "ShenPiHJ": "部门负责人",
+            "ShenPiYJ": "同意(自动审核)"
+        },
+        {
+            "ChuLiSC": "即时",
+            "ShenPiR": "操作管理员",
+            "ShenPiYS": [],
+            "ShenPiSJ": "2019-06-19 14:41:50",
+            "ShenPiFJ": {},
+            "ShenPiHJ": "填报",
+            "ShenPiYJ": "提交"
         }
     ],
     "YaoSuXX": [
